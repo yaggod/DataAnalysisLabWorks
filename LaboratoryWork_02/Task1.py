98a1a81ec30390087178fb49396937b3c8915b00
yaggod <yaggod@yandex.ru>
2024-09-13 15:42:27 +0300

    finished subtask 1 from task 2

---
+++ LaboratoryWork_02/Task1.py
@@ -0,0 +1,34 @@
+from PIL import Image
+from sys import argv
+
+
+def process_image(image):
+    width, height = image.size
+    bufferImage = Image.new("RGB", image.size)
+    resultImage = Image.new("RGB", (width*4, height))
+    data = image.getdata()
+    resultImage.paste(image)
+
+    for colorIndex in range(3):
+        colorData = [tuple_color_masked(color, colorIndex) for color in data]
+        bufferImage.putdata(colorData)
+        resultImage.paste(bufferImage, (width * (1 + colorIndex), 0))
+    return resultImage
+
+
+def tuple_color_masked(originalColor, maskIndex):
+    r = originalColor[0] if maskIndex == 0 else 0
+    g = originalColor[1] if maskIndex == 1 else 0
+    b = originalColor[2] if maskIndex == 2 else 0
+
+    return (r, g, b)
+
+
+if(len(argv) < 2):
+    print("You should specify filepath")
+    exit()
+
+filePath = argv[1]
+
+with Image.open(filePath) as image:
+   process_image(image).show()
