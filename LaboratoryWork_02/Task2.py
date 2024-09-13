7446ef15efc24621fa709c1902ac28badfec708f
yaggod <yaggod@yandex.ru>
2024-09-13 16:00:06 +0300

    finished subtask 2

---
+++ LaboratoryWork_02/Task2.py
@@ -0,0 +1,26 @@
+from PIL import Image
+from sys import argv
+
+
+def get_total_colors_weights(image : Image):
+    data = image.getdata()
+
+    rgbTotal = [0, 0, 0]
+    for pixel in data:
+        for colorIndex in range(0, 3):
+            rgbTotal[colorIndex] += pixel[colorIndex]
+    
+    return rgbTotal
+
+
+if(len(argv) < 2):
+    print("You should specify filepath")
+    exit()
+
+filePath = argv[1]
+
+with Image.open(filePath) as image:
+    colorsNames = ["Red", "Green", "Blue"]
+    totalWeights = get_total_colors_weights(image)
+    maxValueIndex = list.index(totalWeights, max(totalWeights))
+    print(f"Most of the colors in the image are {colorsNames[maxValueIndex]}")
