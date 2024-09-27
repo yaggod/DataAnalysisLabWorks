b9e2d11d7c80afa319c3f5f1856e1ebf2f1216fa
yaggod <yaggod@yandex.ru>
2024-09-27 15:31:12 +0300

    finished task1

---
+++ LaboratoryWork_04/Task1.py
@@ -0,0 +1,15 @@
+from argparse import ArgumentParser
+from moviepy.editor import VideoFileClip
+
+parser = ArgumentParser()
+
+parser.add_argument("--file", required=True, help="File to analyze")
+parser.add_argument("--start", required=True, help="Starting position")
+parser.add_argument("--end", required=True, help="Ending position")
+parser.add_argument("--outputFile", default="result.mp4", help="Output file name, default: result.mp4")
+args = parser.parse_args() 
+
+videoFile = VideoFileClip(args.file)
+resultClip = videoFile.subclip(args.start, args.end)
+
+resultClip.write_videofile(args.outputFile)
