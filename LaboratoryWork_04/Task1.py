5c7d4537e17d74052e7b57baee17803d4356e41b
yaggod <yaggod@yandex.ru>
2024-09-27 15:37:43 +0300

    use with wrapper

--- LaboratoryWork_04/Task1.py
+++ LaboratoryWork_04/Task1.py
@@ -9,7 +9,7 @@ parser.add_argument("--end", required=True, help="Ending position")
 parser.add_argument("--outputFile", default="result.mp4", help="Output file name, default: result.mp4")
 args = parser.parse_args() 
 
-with VideoFileClip(args.file) as videoFile:# = VideoFileClip(args.file)
+with VideoFileClip(args.file) as videoFile:
     resultClip = videoFile.subclip(args.start, args.end)
 
     resultClip.write_videofile(args.outputFile)
