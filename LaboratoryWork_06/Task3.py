24a0a1462e94e7fd1f275e5decc15f2c0b831f7d
yaggod <yaggod@yandex.ru>
2024-11-04 20:14:22 +0300

    finished task3

---
+++ LaboratoryWork_06/Task3.py
@@ -0,0 +1,19 @@
+from argparse import ArgumentParser
+from xml.etree import ElementTree
+
+
+parser = ArgumentParser()
+parser.add_argument("--file", type=str, default="LaboratoryWork_06/inputFiles/ex_3.xml", help="File to read")
+args = parser.parse_args()
+
+
+
+xmlToRead = ElementTree.parse(args.file)
+fileRoot = xmlToRead.getroot()
+
+products = fileRoot.findall("Документ/ТаблСчФакт/СведТов")
+for product in products:
+    print(f"Name: {product.get("НаимТов")}")
+    print(f"Count: {product.get("КолТов")}")
+    print(f"Price: {product.get("ЦенаТов")}")
+    print()
