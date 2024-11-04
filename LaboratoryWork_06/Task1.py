0ef236b4da29166005207b74173ef5c3ae51d050
yaggod <yaggod@yandex.ru>
2024-11-04 19:10:58 +0300

    finished task 1

---
+++ LaboratoryWork_06/Task1.py
@@ -0,0 +1,17 @@
+from argparse import ArgumentParser
+from xmlschema import XMLSchema
+
+parser = ArgumentParser()
+parser.add_argument("--file", type=str, default="inputFiles/ex_1.xml", help="File to validate")
+parser.add_argument("--schema", type=str, default="task1Schema.xsd", help="Validation schema")
+args = parser.parse_args()
+
+
+try:
+    validationSchema = XMLSchema(args.schema)
+    if validationSchema.is_valid(args.file):
+        print("File is valid")
+    else:
+        raise
+except:
+    print("File is not valid")
